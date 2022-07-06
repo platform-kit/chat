323afab34b8a187500963dd27a75f471857e6f73
PlatformKit <54647524+platform-kit@users.noreply.github.com>
2022-07-06 01:48:18 -0700

    cors

--- server.js
+++ server.js
@@ -1,20 +1,30 @@
+const dotenv = require("dotenv").config();
+
 const express = require("express");
+var cors = require("cors");
+const app = express();
+
 const path = require("path");
 const http = require("http");
-const dotenv = require("dotenv").config();
-var cors = require('cors');
-const app = express();
+
 const server = http.createServer(app);
 const io = require("socket.io")(server);
-var twilio = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+
+var twilio = require("twilio")(
+  process.env.TWILIO_ACCOUNT_SID,
+  process.env.TWILIO_AUTH_TOKEN
+);
 
 // Use CORS middleware
-app.use(cors({
-	allowedOrigins: [process.env.ALLOWED_ORIGIN],
-	methods: ['GET']
-  }));
-  
-
+var allowedOrigins = [process.env.ALLOWED_ORIGIN];
+var corsOptions = {
+  allowedOrigins: allowedOrigins,
+  methods: ["GET"],
+};
+console.log("Cors Policy: \n");
+console.log(corsOptions);
+console.log("\n");
+app.use(cors(corsOptions));
 
 // Server all the static files from www folder
 app.use(express.static(path.join(__dirname, "www")));
@@ -27,8 +37,8 @@ server.listen(PORT, null, () => console.log("Listening on port " + PORT));
 
 // API Endpoint for getting Twilio ICE/Turn/Stun Servers
 var cachedToken = null;
-function getNewToken () {
-  twilio.tokens.create({}, function(err, token) {
+function getNewToken() {
+  twilio.tokens.create({}, function (err, token) {
     if (!err && token) {
       cachedToken = token;
     }
@@ -37,93 +47,109 @@ function getNewToken () {
 // fetch token initially
 getNewToken();
 // refetch new token every 15 mins and save to cache
-setInterval(getNewToken, 1000*60*10);
+setInterval(getNewToken, 1000 * 60 * 10);
 
-app.get('/ice', function (req, res) {
+app.get("/ice", cors(corsOptions), function (req, res) {	
   if (!cachedToken) {
-    res.send(400, 'Problem getting ice servers data from Twilio')
+    res.send(400, "Problem getting ice servers data from Twilio");
   } else {
     res.json(cachedToken.iceServers);
   }
 });
 
 // Terms of Service page
-app.get("/legal", (req, res) => res.sendFile(path.join(__dirname, "www/legal.html")));
+app.get("/legal", (req, res) =>
+  res.sendFile(path.join(__dirname, "www/legal.html"))
+);
 
 // All URL patterns should served with the same file.
-app.get(["/", "/:room"], (req, res) => res.sendFile(path.join(__dirname, "www/index.html")));
+app.get(["/", "/:room"], (req, res) =>
+  res.sendFile(path.join(__dirname, "www/index.html"))
+);
 
 const channels = {};
 const sockets = {};
 
 io.sockets.on("connection", (socket) => {
-	const socketHostName = socket.handshake.headers.host.split(":")[0];
+  const socketHostName = socket.handshake.headers.host.split(":")[0];
 
-	socket.channels = {};
-	sockets[socket.id] = socket;
+  socket.channels = {};
+  sockets[socket.id] = socket;
 
-	console.log("[" + socket.id + "] connection accepted");
-	socket.on("disconnect", () => {
-		for (const channel in socket.channels) {
-			part(channel);
-		}
-		console.log("[" + socket.id + "] disconnected");
-		delete sockets[socket.id];
-	});
+  console.log("[" + socket.id + "] connection accepted");
+  socket.on("disconnect", () => {
+    for (const channel in socket.channels) {
+      part(channel);
+    }
+    console.log("[" + socket.id + "] disconnected");
+    delete sockets[socket.id];
+  });
 
-	socket.on("join", (config) => {
-		console.log("[" + socket.id + "] join ", config);
-		const channel = socketHostName + config.channel;
+  socket.on("join", (config) => {
+    console.log("[" + socket.id + "] join ", config);
+    const channel = socketHostName + config.channel;
 
-		// Already Joined
-		if (channel in socket.channels) return;
+    // Already Joined
+    if (channel in socket.channels) return;
 
-		if (!(channel in channels)) {
-			channels[channel] = {};
-		}
+    if (!(channel in channels)) {
+      channels[channel] = {};
+    }
 
-		for (const id in channels[channel]) {
-			channels[channel][id].emit("addPeer", { peer_id: socket.id, should_create_offer: false });
-			socket.emit("addPeer", { peer_id: id, should_create_offer: true });
-		}
+    for (const id in channels[channel]) {
+      channels[channel][id].emit("addPeer", {
+        peer_id: socket.id,
+        should_create_offer: false,
+      });
+      socket.emit("addPeer", { peer_id: id, should_create_offer: true });
+    }
 
-		channels[channel][socket.id] = socket;
-		socket.channels[channel] = channel;
-	});
+    channels[channel][socket.id] = socket;
+    socket.channels[channel] = channel;
+  });
 
-	const part = (channel) => {
-		// Socket not in channel
-		if (!(channel in socket.channels)) return;
+  const part = (channel) => {
+    // Socket not in channel
+    if (!(channel in socket.channels)) return;
 
-		delete socket.channels[channel];
-		delete channels[channel][socket.id];
+    delete socket.channels[channel];
+    delete channels[channel][socket.id];
 
-		for (const id in channels[channel]) {
-			channels[channel][id].emit("removePeer", { peer_id: socket.id });
-			socket.emit("removePeer", { peer_id: id });
-		}
-	};
+    for (const id in channels[channel]) {
+      channels[channel][id].emit("removePeer", { peer_id: socket.id });
+      socket.emit("removePeer", { peer_id: id });
+    }
+  };
 
-	socket.on("relayICECandidate", (config) => {
-		let peer_id = config.peer_id;
-		let ice_candidate = config.ice_candidate;
-		console.log("[" + socket.id + "] relay ICE-candidate to [" + peer_id + "] ", ice_candidate);
+  socket.on("relayICECandidate", (config) => {
+    let peer_id = config.peer_id;
+    let ice_candidate = config.ice_candidate;
+    console.log(
+      "[" + socket.id + "] relay ICE-candidate to [" + peer_id + "] ",
+      ice_candidate
+    );
 
-		if (peer_id in sockets) {
-			sockets[peer_id].emit("iceCandidate", { peer_id: socket.id, ice_candidate: ice_candidate });
-		}
-	});
+    if (peer_id in sockets) {
+      sockets[peer_id].emit("iceCandidate", {
+        peer_id: socket.id,
+        ice_candidate: ice_candidate,
+      });
+    }
+  });
 
-	socket.on("relaySessionDescription", (config) => {
-		let peer_id = config.peer_id;
-		let session_description = config.session_description;
-		console.log("[" + socket.id + "] relay SessionDescription to [" + peer_id + "] ", session_description);
+  socket.on("relaySessionDescription", (config) => {
+    let peer_id = config.peer_id;
+    let session_description = config.session_description;
+    console.log(
+      "[" + socket.id + "] relay SessionDescription to [" + peer_id + "] ",
+      session_description
+    );
 
-		if (peer_id in sockets) {
-			sockets[peer_id].emit("sessionDescription", {
-				peer_id: socket.id,
-				session_description: session_description,
-			});
-		}
-	});
+    if (peer_id in sockets) {
+      sockets[peer_id].emit("sessionDescription", {
+        peer_id: socket.id,
+        session_description: session_description,
+      });
+    }
+  });
 });
